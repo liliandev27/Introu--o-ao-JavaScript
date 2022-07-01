79e98e502252e7267d12a95cf9714182f6b95661
Lilian <liliandevsistem@gmail.com>
2022-06-30 23:14:38 -0400

    Introdução ao javascript

---
+++ Manipulando um arquivo.js
@@ -0,0 +1,13 @@
+//Como boa prática dê nomes que façam sentido à sua variável;
+
+//var preço = 2;
+
+var desconto = 0.2;
+const PRECO = 2;
+
+var total = PRECO - desconto;
+
+// * Variáveis podem ter o valor alterado;
+// * Constantes não podem ter o valor alterado;
+
+
