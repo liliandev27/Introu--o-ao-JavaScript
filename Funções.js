9db4b16ac5a3eff01df5da251b5ebefc166d3ffa
Lilian <liliandevsistem@gmail.com>
2022-06-30 23:24:36 -0400

    Funções

---
+++ Funções.js
@@ -0,0 +1,6 @@
+function soma(a, b) {
+  console.log(a + b); //muito usado para depurar um código
+  return a + b; //Usado para retornar os valores da funçãp
+}
+
+soma(3, 5); //Chama o resultado da função;
